73f9da1c3c8503a878080a119a1394b4daa921fb
Wiziard38 <mathis.girault38@gmail.com>
2021-03-24 21:28:37 +0100

    Update common.py

--- common.py
+++ common.py
@@ -14,4 +14,29 @@ def choices(e, n):
     """
     return [random.choice(e) for i in range(n)]
 
-def evaluation():
+    
+def evaluation(solution, attempt):
+    """Fonction qui évalue un essai en fonction de la solution de référence.
+    Renvoie un tuple composé du nombre de plots de la bonne couleur bien placés, 
+    et du nombre de plots de la bonne couleur mais mals placés.
+    """
+    (red,white) = (0,0)
+    dict = {}
+    for i in range(len(solution)):
+        if solution[i] == attempt[i]:
+            red += 1
+        elif attempt[i] in dict.values():
+            white += 1
+            del dict[i]
+        else:
+            dict[i] = solution[i]
+    return (red,white)
+                                        
+                    
+def test_evaluation():
+    assert(evaluation("RVBJ","RMOB")) == (1,1)
+    assert(evaluation("RVBJ","RMOR")) == (1,0)
+    assert(evaluation("RVBJ","RVJB")) == (2,2)
+    assert(evaluation("RVBJ","RMOB")) == (1,1)
+    
+test_evaluation()
