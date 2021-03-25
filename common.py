f88124480cb1cf674110203798a7c9650b3756cf
Wiziard38 <mathis.girault38@gmail.com>
2021-03-25 14:04:41 +0100

    Update common.py

--- common.py
+++ common.py
@@ -20,23 +20,41 @@ def evaluation(solution, attempt):
     Renvoie un tuple composé du nombre de plots de la bonne couleur bien placés, 
     et du nombre de plots de la bonne couleur mais mals placés.
     """
-    (red,white) = (0,0)
+    
     dict = {}
-    for i in range(len(solution)):
+    """On va d'abord compter le nombre de rouges, les couleurs sont soit rouges,
+    soit on les intègre dans un dictionnaire qui recense le nombre d'appartition
+    de ces couleurs"""
+    red = 0 
+    for i in range(len(solution)-1,-1,-1): # Parcours de la boucle en sens inverse
         if solution[i] == attempt[i]:
             red += 1
-        elif attempt[i] in dict.values():
-            white += 1
-            del dict[i]
+            # On modifie la chaîne de caractères attempt pour enlever le couleur qui a déjà été décrite comme rouge
+            attempt = attempt[:i] + attempt[i+1:]
+        elif solution[i] in dict.keys(): 
+            # Si la couleur est déjà dans le dictionnaire, on augmente le nombre de cette couleur de 1
+            dict[solution[i]] += 1
         else:
-            dict[i] = solution[i]
-    return (red,white)
-                                        
-                    
+            # Si la couleur est pas encore dans le dictionnaire, on dit qu'elle y est une fois
+            dict[solution[i]] = 1
+    
+    """Ensuite on regarde le nomre de blancs, en étudiant les couleurs encore présentes
+    dans la chaine attempt par rapport à notre dictionnaire"""
+    white = 0 
+    for element in attempt:
+        if element in dict.keys() and dict[element] != 0:
+            white += 1
+            dict[element] -= 0
+    return(red,white)
+
+
 def test_evaluation():
     assert(evaluation("RVBJ","RMOB")) == (1,1)
-    assert(evaluation("RVBJ","RMOR")) == (1,0)
-    assert(evaluation("RVBJ","RVJB")) == (2,2)
-    assert(evaluation("RVBJ","RMOB")) == (1,1)
-    
+    assert(evaluation("RRRR","RRRR")) == (4,0)
+    assert(evaluation("RVBJ","MNOG")) == (0,0)
+    assert(evaluation("RVBJ","JRVB")) == (0,4)
+    assert(evaluation("RVVR","RVRV")) == (2,2)
+    assert(evaluation("RRVV","VVRR")) == (0,4)
+    assert(evaluation("RVRV","VRVR")) == (0,4)
+
 test_evaluation()
