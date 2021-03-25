fe8c946b26ee75a77b21e93d635eb29c05144093
Wiziard38 <mathis.girault38@gmail.com>
2021-03-25 17:44:39 +0100

    Update histogramme.py

--- histogramme.py
+++ histogramme.py
@@ -1,10 +1,19 @@
-
-
+import common
+from random import random.randint
 
 # Ancien code pour tracer un histo
-days = []
-number = []
-
+def trace_histo(n):
+    nbr_essais = []
+    global LENGHT
+    global couleur
+    q = LENGHT *len(couleurs)
+    for i in range(n):
+        count = 0
+        while count == 0:
+            if randint(0,q) == 0:
+                nbr_essais.append(count)
+                
+    
 
 plt.hist(np.array(days), bins=np.array(range(1,368)), weights=np.array(number))
 plt.title("fréquence des jours d'anniversaires") 
