e88d31751adab0768d67c57e55abe64647fb2b9b
Wiziard38 <mathis.girault38@gmail.com>
2021-03-26 16:25:37 +0100

    Update histogramme.py

--- histogramme.py
+++ histogramme.py
@@ -4,16 +4,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 
-def trace_histo(n):
-    list = []
-    p = len(common.COLORS)**common.LENGTH
-    list2 = np.random.geometric(1/p, n)
-    plt.hist(list2, range = (0,8000), bins = 8000, color='yellow')
-    plt.title("Nombres d'essais") 
-    plt.show()
-
-
-def trace_histo2(n): # methode 2
+def trace_histo(n): # methode 2
     list = []
     p = len(common.COLORS)**common.LENGTH
     for i in range(n):
@@ -25,5 +16,13 @@ def trace_histo2(n): # methode 2
                 proba = False
             nbr_essais +=1
     plt.hist(list, range = (0,8000), bins = 8000, color='red')
+    plt.title("Nombres d'essais") 
+    plt.show()
+    
+
+def trace_histo2(n): # methode 2
+    p = len(common.COLORS)**common.LENGTH
+    list2 = np.random.geometric(1/p, n)
+    plt.hist(list2, range = (0,8000), bins = 8000, color='yellow')
     plt.title("Nombres d'essais") 
     plt.show()
