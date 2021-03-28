55ac3cc9405de9bd1f432557c6a9e7f0b6b8c41f
Wiziard38 <mathis.girault38@gmail.com>
2021-03-28 19:33:58 +0200

    Update histogramme.py

--- histogramme.py
+++ histogramme.py
@@ -7,33 +7,16 @@ import sys
 import codebreaker0
 import codemaker0
 import common
+from play import play
 
 
-def play(codemaker, codebreaker):
-    """
-    Fonction principale de ce programme :
-    Fait jouer ensemble le codebreaker et le codemaker donnés en arguments
-    """
-    n_tries = 0
-    codebreaker.init()
-    codemaker.init()
-    evaluation_p = None
-    print('Combinaisons de taille {}, couleurs disponibles {}'.format(common.LENGTH, common.COLORS))
-    while True:
-        attempt = codebreaker.codebreaker(evaluation_p)
-        (red, white) = codemaker.codemaker(attempt)
-        n_tries += 1
-        print("Essai {} : {} ({},{})".format(n_tries, attempt, red, white))
-        evaluation_p = (red, white)
-        if red >= common.LENGTH:
-            print("Bravo ! Trouvé {} en {} essais".format(attempt, n_tries))
-            break
-
-def recup_print(play(codemaker,codebreaker)):
+def recup_print(play, codemaker, codebreaker):
+    """ Fonction qui permet de récuperer du texte qui a été écrit dans la console 
+    python par une fonction donnée en argument"""
     # rediriger stdout dans un buffer :
     sys.stdout = io.StringIO()
     # appel de la fonction qui remplira stdout (donc le buffer)
-    play(codemaker, codebreaker)
+    play(codemaker,codebreaker)
     # récupérer le contenu du buffer :
     s = sys.stdout.getvalue()    
     # fermer le buffer :
@@ -45,34 +28,18 @@ def recup_print(play(codemaker,codebreaker)):
     for e in s:
         if e.isdigit():
             nombre += str(e)
-    return nombre
+    return int(nombre)
 
 
-def histogramme(n):
+def histogram(n):
     list = []
     for i in range(n):
-        list.append(recup_print())
+        list.append(recup_print(play, codemaker0, codebreaker0))
+    print(list)
     plt.hist(list, range = (0,8000), bins = 8000)
     plt.title("Nombres d'essais") 
     plt.show()
 
-
-
-def trace_histo(n):
-    list = []
-    p = len(common.COLORS)**common.LENGTH
-    for i in range(n):
-        proba = True
-        nbr_essais = 0
-        while proba:
-            if random.randint(0,p) == 0:
-                list.append(nbr_essais)
-                proba = False
-            nbr_essais +=1
-    plt.hist(list, range = (0,8000), bins = 8000, color='red')
-    plt.title("Nombres d'essais") 
-    plt.show()
-    
 
 def trace_histo2(n): # methode exacte
     p = len(common.COLORS)**common.LENGTH
