18c5bc2da021c4c1c7a6ca58670a40e0df4b2e46
Wiziard38 <mathis.girault38@gmail.com>
2021-04-02 17:07:03 +0200

    Update play.py

--- play.py
+++ play.py
@@ -72,8 +72,8 @@ def play_human_against_codebreaker(codebreaker):
 
 if __name__ == "__main__":
     import codebreaker2
-    import codemaker1
-    play(codemaker1, codebreaker2)
+    import codemaker2
+    play(codemaker2, codebreaker2)
 
     ##
     #  Faire jouer un humain contre codemaker0.py :
