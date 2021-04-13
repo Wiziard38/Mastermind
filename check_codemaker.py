9bee17bf7be452341d67d762117f5f38f2b3c286
Le-Rico <5555redleader@gmail.com>
2021-04-13 08:04:45 +0200

    Update check_codemaker.py

--- check_codemaker.py
+++ check_codemaker.py
@@ -26,4 +26,32 @@ def check_codemaker(file):
         print("Tout va bien, le codemaker n'a pas triché (de manière visible)")
 
 
-check_codemaker('log')
+#check_codemaker('log')
+
+def check_codemakerv2(file):
+    path = os.getcwd()
+    with open(path + "/logs/" + file + ".txt", "r") as log:
+        # Initialisation :
+        attempt =log.readline().strip()
+        (red,white) = log.readline().strip().split(',')
+        eval = (int(red),int(white))
+        possibles = common.donner_possibles(attempt, eval)
+        
+        while eval != (4,0):
+            attempt =log.readline().strip()
+            (red,white) = log.readline().strip().split(',')
+            eval1 = (int(red),int(white))
+            if attempt not in possibles:
+                (r1,b1)=eval1
+                (r,b)=eval
+                if r1>=r:
+                    print("Le codemaker à triché de manière visible !")
+                    return
+
+            else:
+                common.maj_possibles(possibles, attempt, eval1)
+            eval=eval1
+        print("Tout va bien, le codemaker n'a pas triché (de manière visible)")
+                
+check_codemakerv2('log')
+
